b245c0721f70f207a79be912978a1ffbe800caa4
zCarlos12212 <xxzcarlos2003@gmail.com>
2025-11-18 18:57:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
     if (!header) return;
 
     function createSpooky() {
-        const emojis = ["🎃", "🕷️", "🕸️"];
+        const emojis = ["👟", "👕", "🧥", "👖"]];
         const spooky = document.createElement("div");
         spooky.classList.add("spooky");
         spooky.textContent = emojis[Math.floor(Math.random() * emojis.length)];
@@ -309,3 +309,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Si quieres que el chat abra automáticamente minimizado o centrado, lo puedes controlar aquí.
 });
+
